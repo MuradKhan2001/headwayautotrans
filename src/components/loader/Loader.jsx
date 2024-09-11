189fdb4f5011f498690570b2d812190e1a858a57
Murodxon <94508619+MuradKhan2001@users.noreply.github.com>
2024-09-11 17:36:46 +0500

    first commit

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,14 @@
+import "./loader.scss";
+
+const Loader = () => {
+    return <>
+        <div className="spinner-container">
+            <div className="sloy">
+                <div className="loader"></div>
+            </div>
+
+        </div>
+    </>
+};
+
+export default Loader;
